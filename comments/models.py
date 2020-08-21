ff1ba5347baabbcc720fa0ea376995f21cf15e9a
AAAA <dev@onerbs.com>
2020-08-21 08:51:45 -0500

    :sparkles: Add `comment` model

---
+++ comments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from base.models import BaseModel
+from cards.models import Card
+from comments import encoder, peek
+from users.models import User
+
+
+class Comment(BaseModel):
+	text = models.TextField()
+	history = models.TextField()
+	author = models.ForeignKey(
+		User, models.DO_NOTHING, 'authored_comments', editable=False)
+	target = models.ForeignKey(
+		Card, models.CASCADE, 'comments', editable=False)
+
+	def get_preview(self, max_length=20):
+		text, timestamp = encoder.decode_many(self.history)[-1]
+		return '"%s", %s' % (peek(text, max_length), timestamp)
+
+	def __str__(self):
+		return self.text

0b63274bf1075adc579a587f59fe38eac3a74222
AAAA <dev@onerbs.com>
2020-08-21 08:50:14 -0500

    :zap: Update `users`

--- users/serializers.py
+++ users/serializers.py
@@ -4,9 +4,12 @@ from users.models import User
 
 class UserSerializer(serializer(User, [
 	'first_name', 'last_name', 'username', 'email', 'avatar', 'is_confirmed',
-	'owned_teams', 'teams', 'boards', 'favorite_boards', 'assigned_cards',
-	'authored_comments'
+	'owned_teams', 'member_of', 'owned_boards', 'favorite_boards',
+	'assigned_cards', 'authored_comments'
 ])):
-	POST = serializer(User, [
+	pass
+
+	class POST(serializer(User, [
 		'first_name', 'last_name', 'email', 'password'
-	])
+	])):
+		pass
---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+
+from base import viewset
+from core.generics import create_resource
+from users.models import User
+from users.serializers import UserSerializer
+from users.tasks import send_confirmation_email
+
+
+class UserViewSet(viewset(User, UserSerializer)):
+	def create(self, request, *args, **kwargs):
+		user_model = get_user_model()
+		user = user_model.objects.create_user(
+			request.data.get('email').split('@')[0],
+			request.data.get('email'),
+			request.data.get('password'),
+			first_name=request.data.get('first_name'),
+			last_name=request.data.get('last_name'),
+		)
+		response = create_resource(self, UserSerializer(user))
+		if response.status_code == 200:
+			send_confirmation_email(user)
+		return response
+
+	def get_serializer_class(self):
+		if self.request.method == 'POST':
+			return UserSerializer.POST
+		return UserSerializer

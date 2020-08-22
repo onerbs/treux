43e6b315a03f72d58191ba66c4169982a6207fc3
AAAA <dev@onerbs.com>
2020-08-22 00:55:00 -0500

    :sparkles: add core actions  + user confirmation  + password reset (bare)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 
+from core import views
+
 urlpatterns = [
 	path('auth/', include('core.auth')),
+	path('confirm/', views.user_confirm),
+	path('reset/', views.user_reset_password),
 	path('', include('core.docs')),
 ]

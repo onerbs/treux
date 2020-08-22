f7bad588ab41931d60be2606092ce25163644771
AAAA <dev@onerbs.com>
2020-08-21 23:01:07 -0500

    *

--- core/auth/__init__.py
+++ core/auth/__init__.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
+	path('', include('core.auth.default')),
 	path('token/', include('core.auth.token')),
-	path('default/', include('core.auth.default')),
 ]

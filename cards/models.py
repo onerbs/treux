98b21fca92bad20c95aecf1f80703f6ac9a8b164
AAAA <dev@onerbs.com>
2020-08-22 03:08:14 -0500

    :sparkles: update `card` model

--- cards/models.py
+++ cards/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
-from base.models import NamedModel, extends
+from base.models import BaseModel, extends
 from boards.models import Board
 from users.models import User
 
 
-class List(NamedModel):
+class List(BaseModel):
 	index = models.PositiveIntegerField()
+	title = models.CharField(max_length=100)
 	of_board = models.ForeignKey(Board, models.CASCADE, 'lists')
-	exports = extends(NamedModel, 'index', 'of_board')
+	exports = extends(BaseModel, 'index', 'title', 'of_board')
 
 
-class Card(NamedModel):
+class Card(BaseModel):
 	index = models.PositiveIntegerField()
+	text = models.TextField()
 	of_list = models.ForeignKey(List, models.CASCADE, 'cards')
 	assigned_to = models.ManyToManyField(User, 'assigned_cards')
 	expires_at = models.DateTimeField(null=True, default=None)
-	exports = extends(NamedModel, 'index', 'of_list', 'assigned_to', 'expires_at')
+	exports = extends(
+		BaseModel,
+		'index', 'text', 'of_list', 'assigned_to', 'expires_at'
+	)

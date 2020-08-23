4aba1860b9bbb10a630437662ac09080a85d4c83
AAAA <dev@onerbs.com>
2020-08-22 23:13:21 -0500

    :sparkles: feat: allow fields exclusion   PUT fields will inherit from POST fields   exclude a field with the syntax `'!{field}'`

--- base/__init__.py
+++ base/__init__.py
@@ -19,6 +19,13 @@ def serializer(_model, _post_fields: list, _put_fields: list = None):
 	:param _post_fields: The fields for the POST method.
 	:param _put_fields: The fields for the PUT method. This extends POST.
 	"""
+	if _put_fields := (_post_fields + (_put_fields or [])):
+		_put_fields.sort()
+		for field in _put_fields:
+			if field.startswith('!'):
+				_put_fields.remove(field)
+				_put_fields.remove(field[1:])
+
 	class BaseSerializer(ModelSerializer):
 		Meta = _meta(_model, _model.exports)
 

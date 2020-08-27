93277a52e8840f0d86c4c9fcd35a17736455ea37
AAAA <dev@onerbs.com>
2020-08-27 02:04:43 -0500

    :sparkles: update base models

--- base/models.py
+++ base/models.py
@@ -1,50 +1,38 @@
-from datetime import datetime
 from uuid import uuid4
 
 from django.db import models
 
-from core.responses import *
-
-
-def extends(model, *fields):
-	return model.exports + [*fields]
+from core.responses import WithResponses
 
 
 class BaseModel(models.Model, WithResponses):
+	uuid = models.UUIDField(default=uuid4(), unique=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	deleted_at = models.DateTimeField(null=True, default=None)
-	uuid = models.UUIDField(default=uuid4())
-	exports = ['created_at', 'updated_at', 'uuid']
+	exports = ['uuid', 'created_at', 'updated_at']
 
+	@property
 	def kind(self) -> str:
+		"""The readable name of the resource."""
 		return self.__class__.__name__
 
-	def delete(self, /, is_staff=False, force=False, **kwargs):
-		if force is True:
-			if not is_staff:
-				return staff_only()
-			super().delete(**kwargs)
-			return self.deleted(hard=True)
-
-		if self.deleted_at is None:
-			self.deleted_at = datetime.now()
-			self.save(**kwargs)
-			return self.deleted()
-		else:
-			self.deleted_at = None
-			self.save(**kwargs)
-			return self.undeleted()
-
-	def undelete(self, is_staff=False, **kwargs):
-		if not is_staff:
-			return staff_only()
-		self.deleted_at = None
-		self.save(**kwargs)
-		return self.undeleted()
-
 	def rotate_uuid(self):
+		"""Assign a new uuid to this resource."""
 		self.uuid = uuid4()
+
+	@classmethod
+	def get(cls, **kwargs):
+		"""Get a resource by identifier key."""
+		return cls.objects.filter(deleted_at=None).get(**kwargs)
+
+	@classmethod
+	def alive(cls, **kwargs):
+		"""Filter the alive resources by the provided criteria."""
+		return cls.objects.filter(deleted_at=None, **kwargs)
+
+	def __str__(self):
+		return str(self.uuid)
 
 	class Meta:
 		abstract = True
@@ -53,10 +41,7 @@ class BaseModel(models.Model, WithResponses):
 class NamedModel(BaseModel):
 	name = models.CharField(max_length=150)
 	description = models.TextField(blank=True, default='')
-	exports = extends(BaseModel, 'name', 'description')
-
-	def __str__(self):
-		return self.name
+	exports = BaseModel.exports + ['name', 'description']
 
 	class Meta:
 		abstract = True

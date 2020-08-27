af152f995e99a47ef6194d8324b42faa76074673
AAAA <dev@onerbs.com>
2020-08-27 02:55:46 -0500

    :sparkles: base viewset to its own file

--- base/__init__.py
+++ base/__init__.py
@@ -1,10 +1,5 @@
-from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import ModelSerializer
-from rest_framework.viewsets import GenericViewSet
 
-from core.decorators import check_fields
-from core.responses import *
 
 
 def serializer(_model, _post_fields: list, _put_fields: list = None):
@@ -41,58 +36,3 @@ def _serializer(_model, _fields):
 	class _Serializer(ModelSerializer):
 		Meta = _meta(_model, _fields)
 	return _Serializer
-
-
-# ----------------------------------------------------------------------
-
-
-def viewset(_model, _serializer, _permissions=None):
-	"""
-	Create a set of views for the specified model.
-
-	:param _model: The model.
-	:param _serializer: The model serializer.
-	:param _permissions: The list of permission classes.
-	"""
-	if _permissions is None:
-		_permissions = [IsAuthenticated]
-
-	class _ViewSet(
-		mixins.RetrieveModelMixin,
-		mixins.UpdateModelMixin,
-		mixins.ListModelMixin,
-		GenericViewSet,
-		WithResponses,
-	):
-		queryset = _model.objects.all()
-		serializer_class = _serializer
-		permission_classes = _permissions
-
-		@staticmethod
-		def kind() -> str:
-			return _model.__name__
-
-		def create(self):
-			return error(self.name + ' not created.', HTTP_501_NOT_IMPLEMENTED)
-
-		@check_fields([], ['force'])
-		def destroy(self, request, **kwargs):
-			item = self.get_object()
-			is_staff = request.user.is_staff
-			kwargs.pop('pk')
-			return item.delete(is_staff, force=request.force, **kwargs)
-
-		def get_serializer_class(self):
-			if self.request.method == 'POST':
-				return self.serializer_class.POST
-			if self.request.method == 'PUT':
-				return self.serializer_class.PUT
-			return self.serializer_class
-
-		def get_queryset(self):
-			if self.request.user.is_staff:
-				return super().get_queryset()
-			else:
-				return self.queryset.filter(deleted_at=None)
-
-	return _ViewSet
---
+++ base/views.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from rest_framework.viewsets import GenericViewSet, mixins
+
+from core.decorators import with_fields
+from core.responses import WithResponses
+from users.permissions import Default
+
+
+def viewset(_model, serializer, permissions: list = None):
+	if permissions is None:
+		permissions = [Default]
+
+	class _ViewSet(BaseViewSet):
+		queryset = _model.objects.all()
+		serializer_class = serializer
+		permission_classes = permissions
+		kind = _model.__name__
+
+	return _ViewSet
+
+
+class BaseViewSet(
+	mixins.RetrieveModelMixin,
+	mixins.UpdateModelMixin,
+	mixins.ListModelMixin,
+	GenericViewSet,
+	WithResponses
+):
+	lookup_field = 'uuid'
+	kind = 'Model'
+
+	def create(self, *args, **kwargs):
+		"""Not implemented."""
+		return self.fail(status=501)
+
+	@with_fields([], ['is_forever'])
+	def destroy(self, request, **kwargs):
+		"""Soft delete/undelete or destroy completely a resource.
+
+		**Note**:
+			Only superusers are granted to destroy elements
+			from the database.
+		"""
+		item = self.get_object()
+		kwargs.pop(self.lookup_field)
+		if request.is_forever is True:
+			if not request.user.is_superuser:
+				return self.forbidden(True)
+			item.delete(**kwargs)
+			return self.deleted(hard=True)
+
+		using = kwargs.pop('using')
+		if item.deleted_at is None:
+			item.deleted_at = datetime.now()
+			item.save(using=using)
+			return self.deleted()
+		else:
+			item.deleted_at = None
+			item.save(using=using)
+			return self.undeleted()
+
+	def get_serializer_class(self):
+		method = self.request.method
+		if hasattr(self.serializer_class, method):
+			return getattr(self.serializer_class, method)
+		return self.serializer_class
+
+	def get_queryset(self):
+		if self.request.user.is_staff:
+			return self.queryset
+		else:
+			return self.queryset.filter(deleted_at=None)

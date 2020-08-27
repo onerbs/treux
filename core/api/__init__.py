b6ccacd001b37443fb02ec15b0d4a528b8731ce6
AAAA <dev@onerbs.com>
2020-08-27 06:17:46 -0500

    * relocate `api` module

---
+++ core/api/__init__.py
@@ -0,0 +1,27 @@
+from treux.settings import SITE_URL
+
+# api_root = 'api/v1/'
+api_root = ''
+
+
+def api_path(path: str) -> str:
+	"""Create a /api_root/path path."""
+	url = '/' + api_root + path
+	return url
+
+
+def api_url(path: str) -> str:
+	"""Create a SITE_URL/api_root/path url."""
+	url = SITE_URL + api_path(path)
+	return url
+
+
+def api_urls(items: list) -> list:
+	"""Register the urls under {api_root}.
+
+	:param items: The items to register.
+
+	:return: The api url patterns.
+	"""
+	from django.urls import path, include
+	return [path(api_root, include(item)) for item in items]

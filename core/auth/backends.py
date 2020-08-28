e97d68e736b557c9949a60c4cb4164708cf4a447
AAAA <dev@onerbs.com>
2020-08-27 22:01:15 -0500

    update authentication backend

--- core/auth/backends.py
+++ core/auth/backends.py
@@ -3,11 +3,11 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class EmailOrUsernameModelBackend(ModelBackend):
-	def authenticate(self, request, identifier=None, password=None, **kwargs):
-		if '@' in identifier:
+	def authenticate(self, request, username=None, password=None, **kwargs):
+		if '@' in username:
 			user_model = get_user_model()
 			try:
-				identifier = user_model.objects.get(email=identifier).username
+				username = user_model.objects.get(email=username).username
 			except user_model.DoesNotExist as ex:
 				raise ex
-		return super().authenticate(request, identifier, password, **kwargs)
+		return super().authenticate(request, username, password, **kwargs)

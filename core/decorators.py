b952e7b350651956b22b46fe2fa8c0a971513405
AAAA <dev@onerbs.com>
2020-08-27 22:02:50 -0500

    update decorators

--- core/decorators.py
+++ core/decorators.py
@@ -1,72 +1,89 @@
 import functools
-from datetime import datetime
 
-from core.responses import error
+from core.responses import WithResponses, Response
+from core.util import parse_value
+
+self = WithResponses()
 
 
-def with_reference(model, name: str = None):
+def missing(field: str) -> Response:
+	return self.fail(f'Missing {field}')
+
+
+def without_login(fn):
+	@functools.wraps(fn)
+	def enhanced(*args):
+		if args[-1].user.is_authenticated:
+			return self.accept('Already logged in')
+		return fn(*args)
+
+	return enhanced
+
+
+def with_dependency(model):
 	"""Error if the provided uuid isn't related to an existing object."""
-	def bridge(function):
-		@functools.wraps(function)
-		def decorated(*args):
-			request = args[-1]
-			model_name = model.__name__
-			if not (uuid := request.data.get('uuid')):
-				return error(f'Missing UUID')
-			try:
-				instance = model.objects.get(uuid=uuid)
-			except:
-				return error(f'Broken {model_name} reference.')
-			setattr(request, name or model_name.lower(), instance)
-			return function(*args[:-1], request)
 
-		return decorated
+	def bridge(fn):
+		@functools.wraps(fn)
+		def enhanced(*args, **kwargs) -> Response:
+			request = args[-1]
+			field = 'uuid'  # !! uuid.
+			kind = model.__name__.lower()
+			if not (_value := request.data.get(field)):
+				return missing(field)
+			try:
+				item = model.find(**{field: _value})
+			except:
+				return self.fail(f'Wrong {kind} {field}')
+			setattr(request, kind, item)
+			return fn(*args[:-1], request, **kwargs)
+
+		return enhanced
 
 	return bridge
 
 
-def check_fields(required: list, optional: list = None):
+def with_fields(required: list, optional: list = None):
 	"""Error if missing required fields.
 	Also injects the named fields into request."""
 
-	def wrapper(function):
-		@functools.wraps(function)
-		def decorated(*args, **kwargs):
-			request, missing_field = _inject(args[-1], required)
-			if missing_field:
-				return error(f'Missing {missing_field}')
-			request, _ = _inject(request, optional)
-			return function(*args[:-1], request, **kwargs)
+	def bridge(fn):
+		@functools.wraps(fn)
+		def enhanced(*args, **kwargs) -> Response:
+			request, field = _inject(args[-1], 'data', required)
+			if field is not None:
+				return missing(field)
+			request, _ = _inject(request, 'data', optional)
+			return fn(*args[:-1], request, **kwargs)
 
-		return decorated
+		return enhanced
 
-	return wrapper
+	return bridge
 
 
-def _inject(request, fields: list) -> tuple:
+def with_params(params: list = None):
+	"""Inject the named query params into request."""
+
+	def bridge(fn):
+		@functools.wraps(fn)
+		def enhanced(*args, **kwargs) -> Response:
+			request, field = _inject(args[-1], 'query_params', params)
+			if field is not None:
+				return missing(field)
+			return fn(*args[:-1], request, **kwargs)
+
+		return enhanced
+
+	return bridge
+
+
+def _inject(request, store_name: str, fields: list) -> tuple:
 	if not fields:
 		return request, None
 	missing_field = None
+	store = getattr(request, store_name)
 	for field in fields:
-		if not (value := request.data.get(field)):
+		if not (value := store.get(field, '')):
 			missing_field = field
-		if hasattr(request, field):
-			return error(f'PollutionError: {field}')
-		setattr(request, field, _parse(field, value))
+		setattr(request, field, parse_value(field, value))
 	return request, missing_field
-
-
-def _parse(name: str, value: str):
-	if not value:
-		return None
-	if name.startswith('is_'):
-		return value.lower() in ['on', 'true', '1']
-	if name.endswith('_at'):
-		if value.endswith('Z'):
-			value = value.replace('Z', '+00:00')
-		elif 'Z' in value:
-			value = value.replace('Z', '')
-		return datetime.fromisoformat(value)
-	elif name in ['index', 'id'] or name.endswith('_id'):
-		return int(value)
-	return value

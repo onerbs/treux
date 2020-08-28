1a5ff01d25226561a075b2cd4de498320048624b
AAAA <dev@onerbs.com>
2020-08-27 22:01:42 -0500

    add JWT verify endpoint

--- core/auth/jwt.py
+++ core/auth/jwt.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views \
-	import TokenObtainPairView, TokenRefreshView
+	import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
 	path('', TokenObtainPairView.as_view(), name='jwt'),
 	path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+	path('verify/', TokenVerifyView.as_view(), name='verify'),
 ]
